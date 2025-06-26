b38e1f00a5625e8e8ace5a02f4c20cb4c3afe53d
Chris <chriscottone1@gmail.com>
2025-06-26 17:41:36 -0400

    created the WSJTX-Parser class and gave it methods to listen & parse raw UDP packet data. Refactored main.py to utilize newly created classes.

--- classes.py
+++ classes.py
@@ -1,10 +1,61 @@
+import socket
+import struct
+from dataclasses import dataclass
+
+data_motherload = []
+convo = {
+
+}
+
+@dataclass
 class Packet:
-    def __init__(self, packet_type: int, schema: int, program: str, snr: int, delta_time: float, frequency: int,
-                 message: str):
-        self.snr = snr #
-        self.delta_time = delta_time
-        self.frequency = frequency
-        self.message = message
-        self.schema = schema
-        self.program = program
-        self.packet_type = packet_type
+    snr: int #
+    delta_time: float
+    frequency: int
+    message: str
+    schema: int
+    program: str
+    packet_type: int
+
+
+# Make class that does the UDP parsing
+class WsjtxParser:
+    def __init__(self, packet_list: list=None):
+        self.packet_list = packet_list if packet_list is not None else []
+
+    def parse(self, host, port):
+        udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            udp_socket.bind((host, port))
+            print(f"Listening on {host}:{port}...")
+            ans = input("Begin packet parsing? (Y/n)\n").lower()
+            if ans == "n":
+                print("Quitting...")
+                exit()
+            if ans == "y":
+                print("Parsing packets...")
+                while True:
+                    data, addr = udp_socket.recvfrom(1024)
+                    if len(data) >= 12:
+                        self.parse_packet(data=data)
+        except socket.error as msg:
+            print(f"Socket error: {msg}. Could not listen on {host}:{port}.")
+
+
+    def parse_packet(self, data):
+        message_type = struct.unpack(">I", data[8:12])[0]
+        match message_type:
+            case 2:  # Message packets
+                schema = struct.unpack('>I', data[4:8])[0]
+                program = struct.unpack('>6s', data[16:22])[0].decode('utf-8')
+                snr = struct.unpack(">i", data[27:31])[0]
+                time_delta = struct.unpack(">d", data[31:39])[0]
+                fq_offset = struct.unpack('>i', data[39:43])[0]
+                msg = data[52:-2]
+                decoded_msg = msg.decode('utf-8')
+                parsed_packet = Packet(packet_type=message_type, schema=schema, program=program, snr=snr,
+                                                   delta_time=time_delta, frequency=fq_offset, message=decoded_msg)
+                print(parsed_packet.message)
+                self.packet_list.append(parsed_packet)
+            case 1:  # Status packets
+                pass
--- main.py
+++ main.py
@@ -1,36 +1,11 @@
-import socket
-import struct
-from classes import *
+from classes import WsjtxParser, Packet
 
 HOST = '127.0.0.1'
 PORT = 2237
 
-udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+parser = WsjtxParser()
 
-try:
-    udp_socket.bind((HOST, PORT))
-    print(f"Listening on {HOST}:{PORT}")
-except socket.error as msg:
-    print(f"Socket error: {msg}. Could not listen on {HOST}:{PORT}.")
-
-while True:
-    data, addr = udp_socket.recvfrom(1024)
-    if len(data) >= 12:
-        message_type = struct.unpack(">I", data[8:12])[0]
-        match message_type:
-            case 2: # Message packets
-                schema = struct.unpack('>I', data[4:8])[0]
-                program = struct.unpack('>6s', data[16:22])[0].decode('utf-8')
-                snr = struct.unpack(">i", data[27:31])[0]
-                time_delta = struct.unpack(">d", data[31:39])[0]
-                fq_offset = struct.unpack('>i', data[39:43])[0]
-                msg = data[52:-2]
-                decoded_msg = msg.decode('utf-8')
-                parsed_packet = Packet(packet_type=message_type, schema=schema, program=program, snr=snr,
-                                       delta_time=time_delta, frequency=fq_offset, message=decoded_msg)
-                print(parsed_packet.message)
-            case 1: # Status packets
-                pass
+parser.parse(HOST, PORT)
 
 
 
