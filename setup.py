0ca428af7582dc3761f8c074415e5d406c5a41b8
Chris <chriscottone1@gmail.com>
2025-08-09 17:06:58 -0400

    Added links to docs

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="ft8decoder",
-    version="0.1.0",
+    version="0.1.2",
     description="An FT8 message logger that tracks, translates, and organizes CQs, QSOs, and Misc. messages from live WSJT-X packets.",
     long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     url="https://github.com/ZappatheHackka/ft8decoder",
     packages=find_packages(),
     classifiers=[
         "Development Status :: 3 - Alpha",
+        "Operating System :: OS Independent",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
         "Topic :: Communications :: Ham Radio",
@@ -21,7 +23,6 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
-        "Programming Language :: Python :: 3.14",
     ],
     install_requires=[
         "maidenhead",
@@ -35,4 +36,8 @@ setup(
     author="Christopher Cottone",
     author_email="chriscottone1@gmail.com",
     python_requires=">=3.8",
+    project_urls={
+        "Documentation": "https://zappathehackka.github.io/ft8decoder/",
+        "Source Code": "https://github.com/ZappatheHackka/ft8decoder",
+    }
 )
